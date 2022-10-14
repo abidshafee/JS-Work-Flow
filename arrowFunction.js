98d36bd927b6a11f6aeb48dbb28c737f7823b66a
Shafekul Abid <abidshafee@users.noreply.github.com>
2022-10-14 20:16:33 +0600

    init cngs

--- arrowFunction.js
+++ arrowFunction.js
@@ -1,3 +1,19 @@
+// Conventional Function Declaration
+const square = function(number) {
+    return number*number
+}
+console.log(`Conventional Function: ${square(7)}`)
+
+//Declaring the square function -
+//using arrow function 
+// Just Remove the 'function' Keyword - 
+//Place arrow sign between function parameter and body
+const square2 = (number) => {
+    return number*number
+}
+console.log(`Using Arrow Function: ${square2(9)}`)
+
+// implementation of Arrow Function
 const jobs = [
     {id: 1, isActive: true},
     {id: 2, isActive: false},
