89673b38e332839006d73cff8605b81e785ebcf2
Shafekul Abid <abidshafee@users.noreply.github.com>
2022-10-18 00:26:23 +0600

    gnrl cngs

--- flight-widget-live-with-REST/server.js
+++ flight-widget-live-with-REST/server.js
@@ -1,16 +1,26 @@
-const axios = require("axios");
+const PORT = 8000
+const axios = require("axios").default
+const express = require("express")
+const cors = require('cors')
+const app = express()
+require('dotenv').config()
+app.use(cors())
 
-const options = {
-  method: 'GET',
-  url: 'https://madrid-barajas-airport-flights.p.rapidapi.com/MAD/Arrivals',
-  headers: {
-    'X-RapidAPI-Key': 'ed4add34cbmsh242f6f368cf59e5p140522jsn0eb705b75da6',
-    'X-RapidAPI-Host': 'madrid-barajas-airport-flights.p.rapidapi.com'
-  }
-};
+app.get('/flight', (req, res)=>{
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+    const options = {
+        method: 'GET',
+        url: 'https://madrid-barajas-airport-flights.p.rapidapi.com/MAD/Arrivals',
+        headers: {
+          'X-RapidAPI-Key': 'ed4add34cbmsh242f6f368cf59e5p140522jsn0eb705b75da6',
+          'X-RapidAPI-Host': 'madrid-barajas-airport-flights.p.rapidapi.com'
+        }
+      }
+      
+      axios.request(options).then(function (response) {
+          console.log(response.data);
+      }).catch(function (error) {
+          console.error(error);
+      })
+})
+
